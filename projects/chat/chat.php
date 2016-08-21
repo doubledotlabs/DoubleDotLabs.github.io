9481d00bc76ccfc80d840fd6071a36b3bff1845b
James Fenn <TheAndroidMaster@users.noreply.github.com>
2016-08-21 07:58:57 -0400

    Update chat.php

--- projects/chat/chat.php
+++ projects/chat/chat.php
@@ -3,26 +3,27 @@
 <body>
 
 <?php
+header("Access-Control-Allow-Origin: *");
+
 $userId = $_GET["user"];
 $room = $_GET["room"];
 $name = $_GET["name"];
 $text = $_GET["text"];
 $image = $_GET["image"];
 
-if ($userId == null || $room == null || $name == null || ($text == null && $image == null)) echo "undefined variables";
-else {
+$roomsJson = "";
+
+if (!file_exists("rooms.json")) {
+    $rooms = fopen("rooms.json", "w") or die("unable to create rooms.json");
+    fclose($rooms);
+} else $roomsJson = file_get_contents("rooms.json") or die("unable to open rooms.json");
+
+if ($room != null && $name != null) {
     $fileName = "chats/chat-" . $room . ".json";
 
     $textsJson = "";
 
-    if (!file_exists ($fileName)) {
-        $roomsJson = "";
-
-        if (!file_exists("rooms.json")) {
-            $rooms = fopen("rooms.json", "w") or die("unable to create rooms.json");
-            fclose($rooms);
-        } else $roomsJson = file_get_contents("rooms.json") or die("unable to open rooms.json");
-
+    if (!file_exists($fileName)) {
         $roomJson = json_encode(array('name' => $name, 'id' => $room));
 
         if (strlen($roomsJson) > 0) $roomsJson = substr_replace($roomsJson, "," . $roomJson, strlen($roomsJson) - 1) . "]";
@@ -37,23 +38,27 @@ else {
         fclose($texts);
     } else $textsJson = file_get_contents($fileName) or die("unable to read " . $fileName);
 
-    $textJson = "{}";
-    if ($text != null) {
-        $textJson = json_encode(array('user' => $userId, 'text' => $text, 'date' => date('Y-m-d H:i:s')));
-    } else if ($image != null) {
-        $textJson = json_encode(array('user' => $userId, 'image' => $image, 'date' => date('Y-m-d H:i:s')));
-    }
+    if ($userId != null && ($text != null || $image != null)) {
+        $textJson = null;
+        if ($text != null) {
+            $textJson = json_encode(array('user' => $userId, 'text' => $text, 'date' => date('Y-m-d H:i:s')));
+        } else if ($image != null) {
+            $textJson = json_encode(array('user' => $userId, 'image' => $image, 'date' => date('Y-m-d H:i:s')));
+        }
 
-    if (strlen($textsJson) > 0) $textsJson = substr_replace($textsJson, "," . $textJson, strlen($textsJson) - 1) . "]";
-    else $textsJson = "[" . $textJson . "]";
+        if ($textJson != null) {
+            if (strlen($textsJson) > 0) $textsJson = substr_replace($textsJson, "," . $textJson, strlen($textsJson) - 1) . "]";
+            else $textsJson = "[" . $textJson . "]";
 
-    $texts = fopen($fileName, "w") or die("unable to write to " . $fileName);
+            $texts = fopen($fileName, "w") or die("unable to write to " . $fileName);
 
-    fwrite($texts, $textsJson);
-    fclose($texts);
+            fwrite($texts, $textsJson);
+            fclose($texts);
+        }
 
-    echo $textsJson;
-}
+        echo $textsJson;
+    } else echo $roomsJson;
+} else echo $roomsJson;
 
 ?>
 

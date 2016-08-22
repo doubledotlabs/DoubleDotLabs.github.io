a91e5e7b8b28e3f21b6d160b026836d25f57d3be
James Fenn <TheAndroidMaster@users.noreply.github.com>
2016-08-22 15:01:18 -0400

    Update chat.php

--- projects/chat/chat.php
+++ projects/chat/chat.php
@@ -13,9 +13,13 @@ $image = $_GET["image"];
 $roomsJson = "";
 
 if (!file_exists("rooms.json")) {
+    //create rooms.json if it doesn't exist already
     $rooms = fopen("rooms.json", "w") or die("unable to create rooms.json");
     fclose($rooms);
-} else $roomsJson = file_get_contents("rooms.json") or die("unable to open rooms.json");
+} else {
+    //get its contents if it already exists
+    $roomsJson = file_get_contents("rooms.json") or die("unable to open rooms.json");
+}
 
 if ($room != null) {
     $fileName = "chats/chat-" . $room . ".json";
@@ -24,6 +28,7 @@ if ($room != null) {
 
     if (!file_exists($fileName)) {
         if ($name != null) {
+          //create the chatroom if it doesn't exist and the name is specified
           $roomJson = json_encode(array('name' => $name, 'id' => $room));
 
           if (strlen($roomsJson) > 0) $roomsJson = substr_replace($roomsJson, "," . $roomJson, strlen($roomsJson) - 1) . "]";
@@ -40,16 +45,33 @@ if ($room != null) {
     } else $textsJson = file_get_contents($fileName) or die("unable to read " . $fileName);
 
     if ($userId != null && ($text != null || $image != null)) {
+        //verify the user token and replace the temporary id with a real one
+        $curl = curl_init("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" . $userId);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        $data = curl_exec($curl);
+        curl_close($curl);
+
+        $json = json_decode($data);
+        $userId = $json->sub;
+
         $textJson = null;
         if ($text != null) {
+            //make a text post
             $textJson = json_encode(array('user' => $userId, 'text' => $text, 'date' => date('Y-m-d H:i:s')));
         } else if ($image != null) {
+            //make an image post
             $textJson = json_encode(array('user' => $userId, 'image' => $image, 'date' => date('Y-m-d H:i:s')));
         }
 
         if ($textJson != null) {
-            if (strlen($textsJson) > 0) $textsJson = substr_replace($textsJson, "," . $textJson, strlen($textsJson) - 1) . "]";
-            else $textsJson = "[" . $textJson . "]";
+            if (strlen($textsJson) > 0) {
+                //json already exists, add to existing stuff
+                $textsJson = substr_replace($textsJson, "," . $textJson, strlen($textsJson) - 1) . "]";
+            } else {
+                //json doesn't exist yet, make a new array'
+                $textsJson = "[" . $textJson . "]";
+            }
 
             $texts = fopen($fileName, "w") or die("unable to write to " . $fileName);
 
@@ -58,7 +80,7 @@ if ($room != null) {
         }
 
         echo $textsJson;
-    } else echo $roomsJson;
+    } else echo $textsJson;
 } else echo $roomsJson;
 
 ?>
